f750b01fdcf5cb10839f43c5f91367ab941bfcde
apiu12322 <apiu12322@gmail.com>
2020-06-17 22:37:40 +0700

    add bai kiem_tra_bootstrap

---
+++ kiem_tra_bootstrap/1.js
@@ -0,0 +1,58 @@
+function link_click1(){
+	var	nd= document.getElementById('divnoidung1');
+	console.log(nd);
+	// nd.classList.add('hieuung1');
+	if(nd.style.height=="150px"){
+		nd.style.height="0px";
+	}
+	else{
+		nd.style.height="150px";
+	}
+}
+function link_click2(){
+	var	nd= document.getElementById('divnoidung2');
+	console.log(nd);
+	// nd.classList.add('hieuung1');
+	if(nd.style.height=="150px"){
+		nd.style.height="0px";
+	}
+	else{
+		nd.style.height="150px";
+	}
+}
+function link_click3(){
+	var	nd= document.getElementById('divnoidung3');
+	console.log(nd);
+	// nd.classList.add('hieuung1');
+	if(nd.style.height=="150px"){
+		nd.style.height="0px";
+	}
+	else{
+		nd.style.height="150px";
+	}
+}
+function link_click4(){
+	var	nd= document.getElementById('divnoidung4');
+	console.log(nd);
+	// nd.classList.add('hieuung1');
+	if(nd.style.height=="150px"){
+		nd.style.height="0px";
+	}
+	else{
+		nd.style.height="150px";
+	}
+}
+
+// function dapan_click(){
+// 	var	nd= document.getElementById('khungdapan1');
+// 	console.log(nd);
+// 	// nd.classList.add('hieuung1');
+// 	if(nd.style.height=="580px"){
+// 		nd.style.height="0px";
+// 		nd.style.border="0px";
+// 	}
+// 	else{
+// 		nd.style.height="580px";
+// 		nd.style.border="1px solid grey";
+// 	}
+// }
